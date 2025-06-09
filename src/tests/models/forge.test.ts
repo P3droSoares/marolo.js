e0c102580f37f67b082036eb257d88addb2c67d5
P3droSoares <pedro.soares.contato@hotmail.com>
2025-06-09 12:21:40 -0300

    remove os teste automatizados

--- src/tests/models/forge.test.ts
+++
@@ -1,6 +0,0 @@
-import { MaroloJs } from "@/index";
-import { basicTemplate } from "@/templates/basic";
-
-test("Gerar esquema de arquivos e pasta com Forge", () => {
-  MaroloJs.scaffold(basicTemplate, "./gasola-app");
-});
